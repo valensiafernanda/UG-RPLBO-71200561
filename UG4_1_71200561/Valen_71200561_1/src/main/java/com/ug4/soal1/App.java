47328aac1bbedd742c72886215c542d30ce0145f
valensiafernanda <78332934+valensiafernanda@users.noreply.github.com>
2022-09-22 18:59:17 +0700

    Add files via upload

---
+++ UG4_1_71200561/Valen_71200561_1/src/main/java/com/ug4/soal1/App.java
@@ -0,0 +1,13 @@
+package com.ug4.soal1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
