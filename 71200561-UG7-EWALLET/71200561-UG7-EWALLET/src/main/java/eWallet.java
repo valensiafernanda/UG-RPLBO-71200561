4fc5ed7d73dec5714e72a96cf9fc970d4bce34c3
valensiafernanda <78332934+valensiafernanda@users.noreply.github.com>
2022-10-17 19:50:45 +0700

    Update eWallet.java

--- 71200561-UG7-EWALLET/71200561-UG7-EWALLET/src/main/java/eWallet.java
+++ 71200561-UG7-EWALLET/71200561-UG7-EWALLET/src/main/java/eWallet.java
@@ -2,32 +2,87 @@ public class eWallet {
     private User user;
     private int saldo;
 
-    public eWallet(User user){
-
+    public eWallet(User user) {
+        this.user = new User(user.getNama(), user.getEmail(), user.getUang());
     }
 
-    public String topup(int jumlah){
-
+    public void topup(int jumlah) {
+        if(jumlah < 0) {
+            System.out.println("Maaf, jumlah salah!");
+            return;
+        }
+        if(this.user.getUang() < jumlah) {
+            System.out.println("Maaf, uang cash kamu tidak mencukupi! ("+ Main.formatRupiah(this.user.getUang()-jumlah)+")");
+            return;
+        }
+        this.user.topup(jumlah);
+        this.saldo += jumlah;
+        System.out.println("Halo, " + this.getUser().getNama() + "! Top up saldo sebesar " + Main.formatRupiah(jumlah) + " berhasil!");
     }
 
-    public void transfer(eWallet eWallet, int jumlah){
-
+    public void pay(int jumlah) {
+        if(jumlah < 0) {
+            System.out.println("Maaf, jumlah salah!");
+            return;
+        }
+        if(jumlah > this.saldo) {
+            System.out.println("Maaf, saldo kamu tidak mencukupi! ("+ Main.formatRupiah(this.saldo-jumlah)+")");
+            return;
+        }
+        this.saldo -= jumlah;
     }
 
-    public void withdraw(int jumlah){
-
+    public void transfer(eWallet eWallet, int jumlah) {
+        if(jumlah < 0) {
+            System.out.println("Maaf, jumlah salah!");
+            return;
+        }
+        if(jumlah > this.saldo) {
+            System.out.println("Maaf, saldo kamu tidak mencukupi! ("+ Main.formatRupiah(this.saldo-jumlah)+")");
+            return;
+        }
+        this.saldo -= jumlah;
+        eWallet.terima(jumlah);
+        System.out.println("Transfer saldo sejumlah " + Main.formatRupiah(jumlah) + " ke akun " + eWallet.getUser().getNama() + " berhasil!");
     }
 
-    public void getInfo(){
+    private void terima(int jumlah) {
+        if(jumlah < 0) {
+            System.out.println("Maaf, jumlah salah!");
+            return;
+        }
+        this.saldo += jumlah;
+    }
 
+    public void withdraw(int jumlah) {
+        if(jumlah < 0) {
+            System.out.println("Maaf, jumlah salah!");
+            return;
+        }
+        if(jumlah > this.saldo) {
+            System.out.println("Maaf, saldo kamu tidak mencukupi! ("+ Main.formatRupiah(this.saldo-jumlah)+")");
+            return;
+        }
+        this.saldo -= jumlah;
+        this.user.withdraw(jumlah);
     }
 
     public User getUser() {
+        return user;
     }
 
-    protected int getSaldo() {
+    public int getSaldo() {
+        return saldo;
     }
 
-    protected void pay(int feeTransfer) {
+    public void getInfo() {
+        System.out.println("Nama: " + this.user.getNama() + " [PIN: " + this.user.getPIN() + "]");
+        String email = "";
+        if(this.user.isEmailConfirmed()) email = "[Confirmed]";
+        System.out.println("Email: " + this.user.getEmail() + " " + email);
+        System.out.println("Uang cash: " + Main.formatRupiah(this.user.getUang()));
+        System.out.println("Saldo e-wallet: " + Main.formatRupiah(this.getSaldo()));
+        System.out.println();
     }
+
 }
