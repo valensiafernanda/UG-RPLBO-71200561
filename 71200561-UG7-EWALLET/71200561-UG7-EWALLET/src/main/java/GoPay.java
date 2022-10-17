93de8b3082c40a7c6013ce3f10b22950320375ac
valensiafernanda <78332934+valensiafernanda@users.noreply.github.com>
2022-10-17 19:49:22 +0700

    Update GoPay.java

--- 71200561-UG7-EWALLET/71200561-UG7-EWALLET/src/main/java/GoPay.java
+++ 71200561-UG7-EWALLET/71200561-UG7-EWALLET/src/main/java/GoPay.java
@@ -5,50 +5,55 @@ public class GoPay extends eWallet {
     private int feeTransfer = 500;
     private int feeWithdraw = 2500;
 
-    public GoPay(User user){
+    public GoPay(User user) {
         super(user);
     }
 
-    public String topup(int jumlah){
-        if(jumlah > 10000) {
+    public void topup(int jumlah) {
+        if(jumlah < 10000) {
             System.out.println("Maaf, minimal top up Rp10.000,00");
             return;
         }
-        if(jumlah + feeTopup > super.getUser().getUang()){
+        if(jumlah + feeTopup > super.getUser().getUang()) {
             jumlah += feeTopup;
-            System.out.println("Maaf, uang cash anda tidak mencukupi! ("+ Main.formatRupiah(this.getSaldo()-jumlah)+")");
+            System.out.println("Maaf, uang cash kamu tidak mencukupi! ("+ Main.formatRupiah(this.getSaldo()-jumlah)+")");
             return;
         }
         super.topup(jumlah);
         this.pay(feeTopup);
     }
 
+    public void transfer(eWallet eWallet, int jumlah) {
+        System.out.print("Masukkan PIN kamu: ");
+        Scanner input = new Scanner(System.in);
+        String password = input.nextLine();
+        System.out.print("Validasi PIN");
+        Main.tunggu(3);
+        if(!password.equals(this.getUser().getPIN())) {
+            System.out.println("Maaf, PIN yang kamu masukkan salah!");
+            return;
+        }
+        System.out.println("Transfer saldo akan terkena potongan fee " + Main.formatRupiah(feeTransfer));
+        System.out.print("Transfer sedang diproses");
+        Main.tunggu(3);
+        if(jumlah + feeTransfer > super.getSaldo()) {
+            jumlah += feeTransfer;
+            System.out.println("Maaf, saldo kamu tidak mencukupi! ("+ Main.formatRupiah(this.getSaldo()-jumlah)+")");
+            return;
+        }
+        super.transfer(eWallet, jumlah);
+        this.pay(feeTransfer);
+    }
 
-    public void withdraw(int jumlah){
+    public void withdraw(int jumlah) {
         System.out.println("Withdraw saldo akan terkena potongan fee " + Main.formatRupiah(feeWithdraw));
         if(jumlah + feeWithdraw > super.getSaldo()) {
             jumlah += feeWithdraw;
             System.out.println("Maaf, saldo kamu tidak mencukupi! ("+ Main.formatRupiah(this.getSaldo()-jumlah)+")");
             return;
         }
-        System.out.println("Halo, " + this.getUser().getNama() + "! Uang cash sejumlah " + Main.formatRupiah(jumlah) + " telah masuk ke dompetmu!");
         super.withdraw(jumlah);
         this.pay(feeWithdraw);
-    }
-
-    public void transfer(eWallet eWallet, int jumlah){
-        System.out.println("Masukkan PIN anda: ");
-        Scanner input = new Scanner(System.in);
-        String password = input.nextLine();
-        System.out.println("validasi PIN");
-        Main.tunggu(3);
-        if(!password.equals(this.getUser().getPIN())){
-            System.out.println("Maaf, PIN yang anda masukkan salah");
-            return;
-        }
-        System.out.println("transfer saldo akan terkena potongan fee "+ Main.formatRupiah(feeTransfer));
-        System.out.println("transfer sedang diproses");
-        Main.tunggu(3);
     }
 
     public void getInfo() {
@@ -56,3 +61,5 @@ public class GoPay extends eWallet {
         super.getInfo();
     }
 
+
+}
